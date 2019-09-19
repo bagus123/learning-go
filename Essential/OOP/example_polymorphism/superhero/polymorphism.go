6708b56098a95441abcb3934a9d609ece033b343
tubagus anwar <tubagus@dihardjasoftware.com>
2019-09-19 21:57:58 +0700

    add essential OOP

--- Essential/OOP/example_polymorphism/superhero/polymorphism.go
+++ Essential/OOP/example_polymorphism/superhero/polymorphism.go
@@ -2,22 +2,28 @@ package superhero
 
 import "fmt"
 
+// SuperHero is interface
 type SuperHero interface {
 	Attack()
 }
 
+// DoAttack : public function
 func DoAttack(sh SuperHero) {
 	sh.Attack()
 }
 
+// Superman struct
 type Superman struct{}
 
+// Attack : implement interface SuperHero
 func (su Superman) Attack() {
 	fmt.Println("Attach with laser")
 }
 
+// Thor struct
 type Thor struct{}
 
+// Attack : implement interface SuperHero
 func (th Thor) Attack() {
 	fmt.Println("Attach with Hammer")
 }
