01db78c87a855a6e67de0c7d91bf8b5251910663
tubagus anwar <tubagus@dihardjasoftware.com>
2019-09-20 17:23:38 +0700

    add essential Map

--- Essential/Map/example_map_with_struct.go
+++ Essential/Map/example_map_with_struct.go
@@ -1,58 +1,3 @@
-## Map
-
-Map is Collection Object has key and value
-
-#### Example 1 :
-
-file example_map.go
-
-```go
-package main
-
-import "fmt"
-
-func main() {
-
-    mapFruit := make(map[string]int)
-    mapFruit["banana"] = 1
-    mapFruit["orange"] = 2
-    mapFruit["apple"] = 3
-
-    // loop by for range type 1
-    for key := range mapFruit {
-    	fmt.Println("key", key, "value", mapFruit[key])
-    }
-
-    // loop by for range type 2
-    for key, value := range mapFruit {
-    	fmt.Println("key", key, "value", value)
-    }
-
-    // test if entry is present in the map or not
-    value, exist := mapFruit["apple"]
-    if exist {
-    	fmt.Println("key is exist with value ", value)
-    } else {
-    	fmt.Println("key is not exist")
-    }
-
-    //delete an entry
-    delete(mapFruit, "apple")
-    valueAfter, exist := mapFruit["apple"]
-    if exist {
-    	fmt.Println("key is exist with value ", valueAfter)
-    } else {
-    	fmt.Println("key is not exist")
-    }
-
-}
-```
-
-#### Example 2:
-
-file example_map_with_struct.go
-
-```go
 package main
 
 import "fmt"
@@ -114,4 +59,3 @@ func main() {
 	}
 
 }
-```
